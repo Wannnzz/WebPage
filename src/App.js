719789bf0c7292fa3b83b5d792e60fb81036fd5f
Wannnzz <chompoonut2002@gmail.com>
2024-02-06 23:19:34 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HeroSection from "./Component/Hero";
+import Navigation from "./Component/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div>
+    <Navigation/>
+    <HeroSection/>
     </div>
   );
-}
-
+  };
 export default App;
